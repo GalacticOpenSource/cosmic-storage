dfb71679a0cea9c41c1679015f5433ba7ab64e43
01110110 01100001 01110010 01100001 01100100 <varadmodhekar101@gmail.com>
2025-09-20 15:40:45 +0530

    done

--- frontend/cosmic-ui/src/App.jsx
+++ frontend/cosmic-ui/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
+import Header from './components/header/Header'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div id='main'>
+ <Header  />
+    </div>
+  );
 }
 
-export default App
+export default App;
